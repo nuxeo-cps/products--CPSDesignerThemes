eb1b452c8b5c49f1acfa22c34d8cd9313bb12553
Georges Racinet on Ishtar.racinet.fr <georges@racinet.fr>
2010-05-30 20:29:12 +0200

    First round of code inclusion from CPSSkins

--- negociator.py
+++ negociator.py
@@ -23,6 +23,7 @@ import types
 
 from zope.interface import implements
 from zope.component import adapts
+from Acquisition import aq_base, aq_parent, aq_inner
 
 from themecontainer import FSThemeContainer
 
@@ -30,6 +31,11 @@ from OFS.interfaces import IObjectManager
 from zope.publisher.interfaces.http import IHTTPRequest
 
 from Products.CMFCore.utils import getToolByName
+
+CPSSKINS_THEME_COOKIE_ID = 'cpsskins_theme'
+CPSDESIGNER_THEME_COOKIE_ID = 'cpsdesigner_theme'
+CPSSKINS_LOCAL_THEME_ID = '.cpsskins_theme'
+CPSDESIGNER_LOCAL_THEME_ID = '.cps_themes_bindings'
 
 from interfaces import IThemeEngine
 
@@ -57,17 +63,22 @@ class EngineAdapter(object):
         enc = utool.getPortalObject().default_charset
         if enc == 'unicode':
             # XXX maybe better to introspect zpublisher conf
+            # TODO CPSUtil can do that now
             enc = 'utf-8'
         self.encoding = enc
 
     def getThemeAndPageName(self):
         """Return the theme and page that should be rendered.
+        Both can be None, meaning that some downstream (container) defaults
+        may still apply.
+
         To be implemented by subclasses."""
         raise NotImplementedError
 
     def getEngine(self):
         """The fallback is up to the container."""
         theme, page = self.getThemeAndPageName()
+        logger.debug("Requested theme: %r page: %r", theme, page)
         return self.lookupContainer().getPageEngine(
             theme, page, cps_base_url=self.cps_base_url, fallback=True,
             encoding=self.encoding)
@@ -98,18 +109,280 @@ class FixedFSThemeEngine(RootContainerFinder, EngineAdapter):
             'default', 'index', cps_base_url=self.cps_base_url, fallback=True)
 
 class CPSSkinsThemeNegociator(RootContainerFinder, EngineAdapter):
-    """Full negociator that applies CPSSkins rules and uses a root container.
+    """Negociator with same rules as CPSSkins with a root container.
     """
 
-    def getCPSSkinsThemeAndPageName(self):
-        """Pure indirection to CPSSkins."""
-        thmtool = getToolByName(self.context, 'portal_themes')
-        theme, page = thmtool.getRequestedThemeAndPageName(
-            context_obj=self.context, no_defaults=True)
-        logger.debug('CPSSkinsThemeNegociator: requesting (%s,%s)', theme, page)
-        return theme, page
+    def getThemeAndPageName(self, **kw):
+        """Gets the name of the requested theme and page by checking a series
+           of URL parameters, variables, folder attributes, cookies, ...
 
-    getThemeAndPageName = getCPSSkinsThemeAndPageName
+           If no_defaults is True, don't lookup the default theme, return
+           None, None for further treatment instead (used by CPSDesignerThemes)
+           TODO: cleaner to extract this in a common module in CPSUtil
+        """
+
+        REQUEST = self.request
+        FORM = REQUEST.form
+        # selected by writing ?pp=1 in the URL
+        if FORM.get('pp') == '1':
+            return 'printable', None
+
+        # selected by writing ?theme=... in the URL
+        # (+page)
+        theme = FORM.get('theme')
+        page = FORM.get('page')
+        if (theme is not None) or (page is not None):
+            return self._extractThemeAndPageName(theme, page)
+
+        if int(kw.get('editing', 0)) == 1:
+            # session variable (used in edition mode)
+            view_mode = self.getViewMode()
+            theme = view_mode.get('theme')
+            page = view_mode.get('page')
+            if (theme is not None) or (page is not None):
+                return self._extractThemeAndPageName(theme, page)
+
+        # cookie (theme + page)
+        theme = self.getThemeCookie()
+        if theme is not None:
+            return self._extractThemeAndPageName(theme, None)
+
+        # method themes (theme + page)
+        published = REQUEST.get('PUBLISHED')
+        if published is not None:
+            try:
+                published = published.getId()
+            except AttributeError:
+                pass
+            else:
+                theme = self.getThemeByMethod(published)  # TODO ADAPT
+                if theme is not None:
+                    return self._extractThemeAndPageName(theme, None)
+
+        # local theme + page
+        theme = self.getLocalThemeName(**kw)
+        if theme is not None:
+            return self._extractThemeAndPageName(theme, None)
+
+        # nothing requested, it's up to downstream code to handle that
+        return None, None
+
+    @classmethod
+    def _extractThemeAndPageName(self, theme=None, page=None):
+        """Extract the theme and page from the short notation
+        """
+        theme_id = theme
+        page_id = page
+        if theme and '+' in theme:
+            theme_id, page_id = theme.split('+')
+        return theme_id, page_id
+
+    def getThemeCookie(self):
+        """Read a theme from cookies. Return None if none found"""
+        cookies = self.request.cookies
+        theme = cookies.get(CPSDESIGNER_THEME_COOKIE_ID)
+        if theme is None:
+            theme = cookies.get(CPSSKINS_THEME_COOKIE_ID)
+        return theme
+
+    def getRequestPublished(self):
+        # borrowed from CPSPortlets
+        published_obj = self.request.get('PUBLISHED')
+        if published_obj is None:
+            return
+        try:
+            if isinstance(published_obj, types.MethodType):
+                # z3 view?
+                if published_obj.im_func.func_name == 'view_html':
+                    # Convention for default view method name
+                    # XXX Could check request['URL'] too
+                    ob = request['PARENTS'][1]
+                    return ob.getTypeInfo().queryMethodID('view', '')
+            else:
+                return published_obj.getId()
+        except AttributeError:
+            pass
+
+    #
+    # Local theme bindings resolution
+    #
+
+    @classmethod
+    def bottomMostFolder(self, startobj):
+        """Find the bottom-most-folder above startobj.
+
+        Adapted from CPSSkins themes tool.
+        """
+        obj = startobj
+        while 1:
+            if obj.isPrincipiaFolderish and not obj.getId().startswith('.'):
+                return obj
+            parent = aq_parent(aq_inner(obj))
+            if obj is None or parent == obj:
+                return startobj
+            obj = parent
+
+    def getLocalThemeName(self, **kw):
+        """Return the name of a local theme in a given context.
+
+           Adapted from CPSSkins themes tool
+
+           Local themes are obtained from folder attributes, i.e.
+           - as the property of a folder (one theme per line)
+           - as an object located in the folder that is callable and
+             that returns a tuple.
+
+           Local themes are computed by collecting all theme information
+           from the portal to the context folder.
+
+           The format for describing themes is:
+           - simply a string containing the theme id.
+
+           - 'n-m:theme'
+
+           where:
+           - 'theme' is the theme id
+           - (n, m) is a couple with n <= m that describes the interval
+             inside which the theme will be used.
+             (0, 0) means the current folder and all subfolders
+             (1, 0) means all subfolders below the current folder
+             (1, 1) means the subfolders of level 1
+             (0, 1) means the folder and the subfolders of level 1
+             (n, n) means the subfolders of level n
+             ...
+
+           Examples:
+           * with a folder property called '.cpsdesigner_theme' or (BBB)
+           '.cpsskins_theme':
+
+             - lines with intervals:
+
+               0-1:theme1
+               2-4:theme2
+               6-0:theme3
+
+             - string with interval:
+
+               0-1:theme1
+
+             - string without interval:
+
+               theme1
+
+           * with a script called '.cpsdesigner_theme.py' or (BBB)
+           '.cpsskins_theme.py' placed in a folder:
+
+             - tuple with intervals:
+
+               return ('0-1:theme1', '2-4:theme2', '6-0:theme3')
+
+             - string with interval:
+
+               return '0-1:theme'
+
+             - string without interval:
+
+               return 'theme'
+
+        """
+
+        context = self.context
+        bmf = self.bottomMostFolder(context)
+
+        # get themes from the root to current path
+        utool = getToolByName(self.context, 'portal_url')
+        portal = utool.getPortalObject()
+        level = len(utool.getRelativeContentPath(bmf))
+
+        ob = bmf
+        objs = [ob]
+        while True:
+            # move to the parent
+            ob = aq_parent(aq_inner(ob))
+            if ob is None:
+                break
+            objs.append(ob)
+            if ob is portal:
+                break
+
+        # we revert the list since we want to start from the portal and move
+        # to the current object, i.e. local themes if they apply to a given
+        # folder override the themes set in the folders above.
+
+        # XXX GR I'm convinced there's a faster way of doing (starting from
+        # bmf and climbing up). Will check that later if time's available.
+        objs.reverse()
+
+        # get the local theme
+        localtheme = None
+        for obj in objs:
+            theme = self.getLocalTheme(folder=obj, level=level)
+            level -= 1
+            if theme is not None:
+                localtheme = theme
+            # we continue since the local theme can be still be overriden.
+        return localtheme
+
+    @classmethod
+    def getRawLocalThemesSpec(self, folder):
+        """Return the raw object specifying local themes if folder, or None."""
+        folder = aq_base(folder)
+        ob = getattr(folder, CPSDESIGNER_LOCAL_THEME_ID, None)
+        if ob is None: # BBB
+            ob = getattr(folder, CPSSKINS_LOCAL_THEME_ID, None)
+        return ob
+
+    @classmethod
+    def getLocalThemesSpec(self, folder=None, **kw):
+        """Return the list of local themes specifications in a given context.
+        """
+
+        if folder is None:
+            return []
+
+        theme_obj = self.getRawLocalThemesSpec(folder)
+
+        if theme_obj is not None and callable(theme_obj):
+            theme_obj = apply(theme_obj, ())
+        if isinstance(theme_obj, basestring):
+            theme_obj = (theme_obj, )
+        if not isinstance(theme_obj, tuple):
+            return []
+
+        themes = []
+        for l in theme_obj:
+            if ':' not in l:
+                themes.append(((0,0), l))
+                continue
+            nm, theme = l.split(':')
+            if '-' not in nm:
+                continue
+            n, m = nm.split('-')
+            themes.append(((int(n), int(m)), theme))
+        return themes
+
+    def getLocalTheme(self, folder=None, level=None):
+        """ Return the name of the first theme assigned to a given level
+            relative to a folder.
+        """
+
+        if level is None:
+            return None
+
+        themes = self.getLocalThemesSpec(folder=folder)
+        if themes is None:
+            return None
+
+        level = int(level)
+        for (n, m), theme in themes:
+            if n > 0 and n > level:
+                continue
+            if m > 0 and m < level:
+                continue
+            return theme
+
+        return None
+
 
 class CherryPickingCPSSkinsThemeNegociator(CPSSkinsThemeNegociator):
     """CPSSKins negociation, overridden by a property on context object only.
@@ -141,35 +414,13 @@ class CherryPickingCPSSkinsThemeNegociator(CPSSkinsThemeNegociator):
          This is a local version of CPSSKins' "Method Themes"
     """
 
-    def getRequestPublished(self):
-        # borrowed from CPSPortlets
-        published_obj = self.request.get('PUBLISHED')
-        if published_obj is None:
-            return
-        try:
-            if isinstance(published_obj, types.MethodType):
-                # z3 view?
-                if published_obj.im_func.func_name == 'view_html':
-                    # Convention for default view method name
-                    # XXX Could check request['URL'] too
-                    ob = request['PARENTS'][1]
-                    return ob.getTypeInfo().queryMethodID('view', '')
-            else:
-                return published_obj.getId()
-        except AttributeError:
-            pass
-
     def getThemeAndPageName(self):
         prop = self.context.getProperty('.cps_designer_theme', None)
         if prop:
             published, themepage = prop.split(':')
             published = published.strip()
         if not prop or (published and published != self.getRequestPublished()):
-            theme, page = self.getCPSSkinsThemeAndPageName()
+            theme, page = CPSSkinsNegociator.getThemeAndPageName(self)
         else:
             theme, page = tuple(s.strip() for s in themepage.split('+'))
         return theme, page
-
-
-
-
--- tests/test_negociator.py
+++ tests/test_negociator.py
@@ -21,9 +21,14 @@ import unittest
 from Testing import ZopeTestCase
 
 from Acquisition import Implicit
+from OFS.Folder import Folder
 
 from Products.CPSDesignerThemes.themecontainer import FSThemeContainer
 from Products.CPSDesignerThemes.negociator import FixedFSThemeEngine
+from Products.CPSDesignerThemes.negociator import CPSSkinsThemeNegociator
+from Products.CPSDesignerThemes.negociator import (
+    CPSSKINS_LOCAL_THEME_ID, CPSSKINS_THEME_COOKIE_ID
+    )
 
 from Products.CPSDesignerThemes.interfaces import IThemeContainer
 
@@ -37,6 +42,11 @@ class FakeUrlTool(Implicit):
 
     def getBaseUrl(self):
         return '/'
+
+    def getRelativeContentPath(self, content):
+        portal_phy = self.getPortalObject().getPhysicalPath()
+        content_phy = content.getPhysicalPath()
+        return content_phy[len(portal_phy):]
 
 class TestFixedFSThemeEngine(ZopeTestCase.ZopeTestCase):
 
@@ -59,8 +69,151 @@ class TestFixedFSThemeEngine(ZopeTestCase.ZopeTestCase):
         negociator = FixedFSThemeEngine(self.folder, self.app.REQUEST)
         self.assertEquals(negociator.encoding, 'utf-8')
 
+class TestCPSSkinsNegociator(ZopeTestCase.ZopeTestCase):
+    """Adaptation from CPSSkins tests."""
+
+    def afterSetUp(self):
+        self.folder.portal_url = FakeUrlTool().__of__(self.folder)
+        self.portal = self.folder
+        self.folder.default_charset = 'latin-1'
+        self.folder._setObject('container', FSThemeContainer('container'))
+
+        self.REQUEST = self.portal.REQUEST
+        self.REQUEST.SESSION = {}
+        self.REQUEST.cookies = {}
+        self.REQUEST.form = {}
+
+    def getRequestedThemeAndPageName(self, context_obj=None):
+        if context_obj is None:
+            context_obj = self.folder
+
+        neg = CPSSkinsThemeNegociator(context_obj, self.REQUEST)
+        return neg.getThemeAndPageName()
+
+    def test_printable(self):
+        self.REQUEST.form['pp'] = '1'
+        theme = self.getRequestedThemeAndPageName()
+        self.assert_(theme == ('printable', None))
+
+    def test_form_1(self):
+        self.REQUEST.form['theme'] = 'theme'
+        theme = self.getRequestedThemeAndPageName()
+        self.assert_(theme == ('theme', None))
+
+    def test_form_2(self):
+        self.REQUEST.form['theme'] = 'theme+page'
+        theme = self.getRequestedThemeAndPageName()
+        self.assert_(theme == ('theme', 'page'))
+
+    def test_form_3(self):
+        self.REQUEST.form['theme'] = 'theme+page1'
+        self.REQUEST.form['page'] = 'page2'
+        theme = self.getRequestedThemeAndPageName()
+        self.assert_(theme == ('theme', 'page1'))
+
+    def test_cookie_theme_1(self):
+        self.REQUEST.cookies[CPSSKINS_THEME_COOKIE_ID] = 'theme'
+        theme = self.getRequestedThemeAndPageName()
+        self.assert_(theme == ('theme', None))
+
+    def test_cookie_theme_2(self):
+        self.REQUEST.cookies[CPSSKINS_THEME_COOKIE_ID] = 'theme+page'
+        theme = self.getRequestedThemeAndPageName()
+        self.assert_(theme == ('theme', 'page'))
+
+    # Deactivated tests related to the themes editor.
+    # Maybe will have to do something similar later for the portlets editor.
+    def xtest_session_1(self):
+        self.tmtool.setViewMode(theme='theme')
+        theme = self.tmtool.getRequestedThemeAndPageName(editing=1)
+        self.assert_(theme == ('theme', None))
+
+    def xtest_session_2(self):
+        self.tmtool.setViewMode(theme='theme', page='page')
+        theme = self.tmtool.getRequestedThemeAndPageName(editing=1)
+        self.assert_(theme == ('theme', 'page'))
+
+    def xtest_session_3(self):
+        self.tmtool.setViewMode(theme='theme+page1', page='page2')
+        theme = self.tmtool.getRequestedThemeAndPageName(editing=1)
+        self.assert_(theme == ('theme', 'page1'))
+
+    def xtest_session_3(self):
+        # the session info is only read in editing mode
+        theme = self.tmtool.getRequestedThemeAndPageName()
+        self.assert_(theme != ('theme', None))
+
+    def getLocalThemeFolder(self):
+        testfolder = self.makeSubFolder(self.portal, 'treeroot')
+        return self.makeSubFolder(testfolder, 'fold_two')
+
+    def makeSubFolder(self, folder, new_id):
+        sub = Folder(new_id)
+        folder._setOb(new_id, sub)
+        return getattr(folder, new_id)
+
+    def test_local_theme_1(self):
+        folder = self.getLocalThemeFolder()
+        value = 'theme'
+        folder.manage_addProperty(CPSSKINS_LOCAL_THEME_ID, value, 'string')
+        theme = self.getRequestedThemeAndPageName(context_obj=folder)
+        self.assert_(theme == ('theme', None))
+
+    def test_local_theme_2(self):
+        folder = self.getLocalThemeFolder()
+        value = 'theme+page'
+        folder.manage_addProperty(CPSSKINS_LOCAL_THEME_ID, value, 'string')
+        theme = self.getRequestedThemeAndPageName(context_obj=folder)
+        self.assert_(theme == ('theme', 'page'))
+
+    def test_local_theme_3(self):
+        folder = self.getLocalThemeFolder()
+        subfolder = self.makeSubFolder(folder, 'subfolder')
+
+        value = 'theme'
+        folder.manage_addProperty(CPSSKINS_LOCAL_THEME_ID, value, 'string')
+        theme = self.getRequestedThemeAndPageName(context_obj=folder)
+        self.assert_(theme == ('theme', None))
+        theme = self.getRequestedThemeAndPageName(context_obj=subfolder)
+        self.assert_(theme == ('theme', None))
+
+    def test_local_theme_4(self):
+        folder = self.getLocalThemeFolder()
+        subfolder = self.makeSubFolder(folder, 'subfolder')
+
+        value = '1-1:theme'
+        folder.manage_addProperty(CPSSKINS_LOCAL_THEME_ID, value, 'string')
+        theme = self.getRequestedThemeAndPageName(context_obj=folder)
+        self.assertEquals(theme, (None, None))
+        theme = self.getRequestedThemeAndPageName(context_obj=subfolder)
+        self.assertEquals(theme, ('theme', None))
+
+    def test_local_theme_5(self):
+        folder = self.getLocalThemeFolder()
+        subfolder = self.makeSubFolder(folder, 'subfolder')
+
+        value = ['1-1:theme2', '0-0:theme1']
+        folder.manage_addProperty(CPSSKINS_LOCAL_THEME_ID, value, 'lines')
+        theme = self.getRequestedThemeAndPageName(context_obj=folder)
+        self.assert_(theme == ('theme1', None))
+        theme = self.getRequestedThemeAndPageName(context_obj=subfolder)
+        self.assert_(theme == ('theme2', None))
+
+    def test_local_theme_6(self):
+
+        testfolder = self.getLocalThemeFolder()
+
+        value = ['1-0:theme1+page1']
+
+        portal = self.portal
+        portal.manage_addProperty(CPSSKINS_LOCAL_THEME_ID, value, 'lines')
+        theme = self.getRequestedThemeAndPageName(context_obj=portal)
+        self.assert_(theme == (None, None))
+        theme = self.getRequestedThemeAndPageName(context_obj=testfolder)
+        self.assert_(theme == ('theme1', 'page1'))
 
 def test_suite():
     return unittest.TestSuite((
         unittest.makeSuite(TestFixedFSThemeEngine),
+        unittest.makeSuite(TestCPSSkinsNegociator),
         ))
